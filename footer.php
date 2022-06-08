113fb724f5ff348b935da6d65f2b723cee60d1ff
maelbadet <98584091+maelbadet@users.noreply.github.com>
2022-06-08 07:56:33 +0200

    changement
    changement effectuer le 08/06/2022 à 7h56

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-
-    <section id = "logo">
-    <a href="https://www.facebook.com/E.S.E.R.ELEC"><img src="img/logo_facebook.png" id="logo_reso" alt=""></a>
-    <a href="https://www.instagram.com/e.s.e.r.electricite.generale/?hl=fr"><img src="img/logo_insta.png" id="logo_reso" alt=""></a>
-    <a href="#"><img src="img/logo_twitter.png" id="logo_reso" alt=""></a>
-    </section>
-</footer>
